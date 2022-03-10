84bdc1e2f919137d88dea66a301d0fee7943fd6f
vidyashree <vidyajayram1997@gmail.com>
2022-03-10 16:18:30 +0530

    created an array for available categories

--- B18ABookKart/src/app/components/categories/categories.component.ts
+++ B18ABookKart/src/app/components/categories/categories.component.ts
@@ -11,14 +11,16 @@ import { ApiService } from 'src/app/services/api.service';
 export class CategoriesComponent implements OnInit {
   
   public bookList:any=[];
-   public filterCategory:any=[];
-   public priceFilter:any=[];
-   public value: number = 1;
-    noPrice: boolean=false;
-  //  public rate = new BehaviorSubject<any>([""]);
-  price: any;
+  public filterCategory:any=[];
+  public priceFilter:any=[];
+  public value: number = 1;
+  public  noPrice: boolean=false;
+  public  price: any;
   public categoryName:any;
   public rate:any;
+  categoryListArray : any = [
+    {category0:'All category ',category1:'Biography',category2:'Fiction',category3:'Mistery',
+    category4:'Fantasy',category5:'Romance',category6:'Drama'}];
   
 
   constructor(private api:ApiService) { }
@@ -26,10 +28,9 @@ export class CategoriesComponent implements OnInit {
   ngOnInit(): void {
     this.categoryName='';
     this.rate=56000;
-    this.api.getList()
-    .subscribe(res=>{
-      this.bookList = res;
-      this.filterCategory = res;
+    this.api.getList().subscribe(res=>{
+    this.bookList = res;
+    this.filterCategory = res;
       this.priceFilter=res;
     })
   }
@@ -37,10 +38,8 @@ export class CategoriesComponent implements OnInit {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'k';
     }
-
     return value;
   }
-  
   filter(category:string){
     this.categoryName=category;
  this.filterCategory=this.bookList.filter((a:any)=>{
@@ -53,11 +52,8 @@ export class CategoriesComponent implements OnInit {
  this.api.filter.next(this.bookList);
   }
   filter1(){
-    this.filter(this.categoryName)
-
-    
+    this.filter(this.categoryName)  
   }
-  
   }
   
   
